b45c2557738f042dbf07d0456b1448f0aed60768
AlenaRudenko <pineapple001@mail.ru>
2023-03-20 20:06:50 +0300

    updated 3 files

--- src/pages/auth-modal/RegistrationModal.tsx
+++ src/pages/auth-modal/RegistrationModal.tsx
@@ -27,68 +27,65 @@ export const RegistrationModal = ({
   const [login, setLogin] = useState<IState["login"]>("");
   const [email, setEmail] = useState<IState["email"]>("");
   const [password, setPassword] = useState<IState["password"]>("");
-  const [status, setStatus] = useState("");
+  const [isDisabled, setIsDisabled] = useState(false);
   const [confirmPassword, setConfirmPassword] =
     useState<IState["confirmPassword"]>("");
 
   const dispatch = useDispatch<Dispatch>();
   //обработчик ввода логина
   const handleLogin = (e: TEvent) => {
+    setIsDisabled(false);
     setLogin(e.target.value);
   };
   //обработчик ввода email
   const handleEmail = (e: TEvent) => {
+    setIsDisabled(false);
     setEmail(e.target.value);
   };
   //обработчик ввода пароля
   const handlePassword = (e: TEvent) => {
+    setIsDisabled(false);
     setPassword(e.target.value);
   };
   //обработчик повторного ввода пароля
   const handleConfirmPassword = (e: TEvent) => {
+    setIsDisabled(false);
     setConfirmPassword(e.target.value);
   };
+  const handleSubmitForms = (message: string) => {
+    if (message === "USER_ALREADY_EXIST") {
+      setIsDisabled(true);
+    } else setIsDisabled(false);
+  };
   const handleRegistrationData = () => {
-    // Api.register({ fname: login, email, password })
-    //   .then((response) => {
-    //     AuthService.setToken(response.data.tokens);
-    //     Api.setToken(response.data.tokens.accessToken);
-    //     dispatch.user.setUser(response.data.user);
-    //   })
-    //   .catch((error) => console.log("BLYAT", error));
+    Api.register({ fname: login, email, password })
+      .then((response) => {
+        AuthService.setToken(response.data.tokens);
+        Api.setToken(response.data.tokens.accessToken);
+        dispatch.user.setUser(response.data.user);
+      })
+      .catch((error) => handleSubmitForms(error.response.data.message));
   };
-  const onMouseOverListener = (e: any) => {
-    e.stopPropagation();
-    if (!isLogin && login) {
-      const timer = setTimeout(() => {
-        console.log("нихуясе");
-      }, 1);
-    }
-  };
+
   const isEmail = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g).test(email);
   const isLogin = new RegExp(/^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$/g).test(login);
   const isPassword = new RegExp(
     /(?=^.{6,10}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/g
   ).test(password);
+  const isConfirmPassword = password === confirmPassword;
   return (
-    <div className="regpage__container">
-      <div className="regpage__form">
-        <Tooltip text={} />
-        <div
-          className="listener "
-          onPointerLeave={(e) => onMouseOverListener(e)}
-        >
-          <AppInput
-            key={"login"}
-            hasError={!isLogin}
-            errorMessage={"неверный логин"}
-            leftIcon={"UserPlus"}
-            value={login}
-            label={"login"}
-            onChange={handleLogin}
-            placeholder={"введите логин"}
-          />
-        </div>
+    <div className='regpage__container'>
+      <div className='regpage__form'>
+        <AppInput
+          key={"login"}
+          hasError={!isLogin}
+          errorMessage={"неверный логин"}
+          leftIcon={"UserPlus"}
+          value={login}
+          label={"login"}
+          onChange={handleLogin}
+          placeholder={"введите логин"}
+        />
 
         <AppInput
           key={"email"}
@@ -123,18 +120,26 @@ export const RegistrationModal = ({
           placeholder={"повторите пароль"}
         />
       </div>
+
+      <div className='regpage__notification'>
+        {isDisabled && <span>пользователь уже существует</span>}
+      </div>
+
       <div
         onClick={() => {
           handleRegistrationData();
-          handleRegistrationPage();
-          toggleAuthVisible();
+          // handleRegistrationPage();
+          // toggleAuthVisible();
         }}
       >
         <AppButton
-          backgroundColor="blue"
-          isDisabled={false}
-          marginTop={20}
-          text="Зарегистрироваться"
+          marginTop={10}
+          backgroundColor='blue'
+          isDisabled={
+            isDisabled ||
+            !(isEmail && isLogin && isPassword && isConfirmPassword)
+          }
+          text='Зарегистрироваться'
         />
       </div>
     </div>
