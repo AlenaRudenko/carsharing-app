00051d5c3334c144c7e5db45117a65a7e899752a
Alyona Rudenko <alenka-rudenko@mail.ru>
2023-03-22 12:30:52 +0000

    Update 3 files

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,6 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { RootState, Dispatch } from "../../store/store";
 import "./styles.scss";
+import { useNavigate } from "react-router-dom";
+
 interface IProps {
   color?: string;
   size?: string;
@@ -21,6 +23,10 @@ export const Header = ({
   const [city, setCity] = useState("");
   const dispatch = useDispatch<Dispatch>();
   const cityId = useSelector((state: RootState) => state.order.cityId);
+  const navigate = useNavigate();
+  const goHome = () => {
+    navigate("/");
+  };
   const localCity = LocalStore.getCurrentCity();
   useEffect(() => {
     if (!testCity) {
@@ -32,7 +38,9 @@ export const Header = ({
   const hangler = () => {};
   return (
     <div className="header" style={{ color, padding }}>
-      <h1 style={{ fontSize: size }}>Need for Drive</h1>
+      <h1 onClick={goHome} style={{ fontSize: size }}>
+        Need for Drive
+      </h1>
       <DropdownMenu
         testCity={testCity ? testCity : localCity!}
         handleLocalStoreCity={
